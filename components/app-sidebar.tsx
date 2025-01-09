1ccf02806184951df972154a82d86a2332f29954
Aptiq <joey.montani@aptiq.ch>
2025-01-09 22:05:07 +0100

    add a small game (1)

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -93,7 +93,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar 
       collapsible="icon" 
-      collapsed={isCollapsed}
+      isCollapsed={isCollapsed}
       onCollapsedChange={setIsCollapsed}
       {...props}
     >
